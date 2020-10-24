0c34e2f299c0c647f03dd20968a25a2655ea2e70
Aditya Vishwekar <AdityaVishwekar@MacBook-Pro.local>
2020-10-24 11:44:47 -0700

    updated username of rds

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -1,18 +1,19 @@
 resource "aws_db_instance" "my-test-sql" {
   instance_class          = var.db_instance
   engine                  = "postgres"
-  engine_version          = "12.3"
+  engine_version          = "10.7"
   multi_az                = true
   storage_type            = "gp2"
   allocated_storage       = 20
   name                    = "uat_dev"
-  username                = "yourusername"
-  password                = "yoursecurepassword"
+  username                = "postgres"
+  password                = "password"
   apply_immediately       = "true"
   backup_retention_period = 10
   backup_window           = "09:46-10:16"
   db_subnet_group_name    = aws_db_subnet_group.my-rds-db-subnet.name
   vpc_security_group_ids  = [aws_security_group.my-rds-sg.id]
+  skip_final_snapshot     = "true"
 }
 
 resource "aws_db_subnet_group" "my-rds-db-subnet" {
